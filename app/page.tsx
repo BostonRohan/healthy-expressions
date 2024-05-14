f7dc9d71864351e47fb101a0db3c452873182f4f
BostonRohan <bostonrohan@gmail.com>
2024-05-14 00:17:30 -0400

    feat: contact us

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
@@ -117,6 +118,35 @@ export default function Home() {
               </AccordionContent>
             </AccordionItem>
           </Accordion>
+        </section>
+        <section className="max-w-3xl mx-auto sm:pl-8 space-y-4 flex justify-center flex-col text-center">
+          <H2>Want to be apart of the community?</H2>
+          <P>
+            Join our exclusive Facebook group to connect with fellow members,
+            share experiences, and stay updated on the latest news, events, and
+            content.{" "}
+          </P>
+          <Link
+            href="https://www.facebook.com/share/gPWn6ALpHuGHfiDd/?mibextid=lOuIew"
+            target="_blank"
+            className={`${buttonVariants({ variant: "default" })} bg-[#4267B2] self-center hover:bg-[#4254b2]`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-facebook"
+            >
+              <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+            </svg>
+            Join our Facebook Group
+          </Link>
         </section>
       </main>
     </div>
